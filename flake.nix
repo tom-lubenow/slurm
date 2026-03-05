9beb54e07d211ccb18bc2825dbe83b82600d2c7b
Tom Lubenow <tomlubenow@gmail.com>
2026-03-04 17:18:45 -0800

    fix(flake): use public GitHub URL for nixnative input
    Switch from local path to github: scheme now that the repo is public.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixnative.url = "path:/home/tom/git/nixnative";
+    nixnative.url = "github:tom-lubenow/nixnative";
   };
 
   outputs = { self, nixpkgs, nixnative }:
